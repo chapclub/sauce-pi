6524679c22528aefc0ffc3ed8c26e07216d8617d
Andrew Ksendzoff <dacoconuttman@gmail.com>
2018-03-28 22:10:37 -0500

    added Pump and Scale

---
+++ src/lib/Pump.js
@@ -0,0 +1,45 @@
+import { Gpio } from 'onoff'
+import Scale from './Scale'
+
+export class Pump {
+  /**
+   * @constructor
+   * @param pumpPins Ordered array of the pump pins
+   * @param scalePins Object with props 'clock' and 'data'
+   */
+  constructor (pumpPins, scalePins) {
+    this.scale = new Scale(scalePins)
+    this.pumps = []
+    pumpPins.forEach(elem => {
+      this.pumps.push(new Gpio(pumpPins[elem], 'out'))
+    })
+    this.pumps.forEach(pump => {
+      pump.write(0, err => {
+        throw new Error(err)
+      })
+    })
+  }
+
+  /**
+   * @function pump
+   * @argument pump 0-indexed number of the pump
+   */
+  pour (pump, quantity) {
+    let currVolume = 0
+    this.pumps[pump].write(1, err => {
+      throw new Error(err)
+    })
+    while (currVolume < quantity) {
+      setTimeout(() => {
+        currVolume = this.scale.measure()
+      }, 250)
+    }
+    this.pumps[pump].write(0, err => {
+      throw new Error(err)
+    })
+  }
+
+  destroy () {
+    this.pumps.forEach(pump => pump.unexport())
+  }
+}
---
+++ src/lib/Scale.js
@@ -0,0 +1,23 @@
+import HX711 from 'hx711'
+
+export class Scale {
+  /**
+   * @constructor
+   * @param pins Object with props 'clock' and 'data'
+   */
+  constructor (pins) {
+    const { clock, data } = pins
+    this.scale = new HX711(clock, data)
+    this.scale.setScale(-414.89)
+    this.scale.tare()
+    this._initOffset = this.scale.getOffset()
+  }
+
+  measure () {
+    return this.scale.getUnits() + this.scale.getOffset()
+  }
+
+  tare () {
+    return this.scale.tare()
+  }
+}
